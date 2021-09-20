5acd5cc4b937fb6911c66a46b86ea1fdebd5bb36
Ilweran <quizblow@gmail.com>
2021-09-20 08:03:18 -0400

    Some makeup in the output to STDOUT

--- manage_netns.sh
+++ manage_netns.sh
@@ -36,23 +36,35 @@ for f in $NETNS_FILES; do
   ns=$(echo $f | cut -d "/" -f 5)
   if [[ "$ns_list" == *"$ns"* ]]
   then
-    printf "\n******* The $ns network namespace already exists. Checking if re-configuration is needed. *******\n"
+    printf "\n***************************************************************************************\n"
+    printf "\n** The $ns network namespace already exists. Checking if re-configuration is needed. **\n"
+    printf "\n***************************************************************************************\n"
+    tmp_interface=$(ip netns exec $ns ip a | egrep "vlan.*if" | awk '{print $2}' | sed 's/\@.*//')
+    tmp_vlan=$(ip netns exec $ns ip a | egrep "vlan.*if" | awk '{print $2}' | sed 's/vlan\.//' | sed 's/\@.*//')
+    tmp_ipaddress=$(ip netns exec $ns ip a | egrep "inet 192" | awk '{print $2}')
     while read line; do
     interface=$(echo $line | awk '{print $1}')
     vlan=$(echo $line | awk '{print $2}')
     ipaddress=$(echo $line | awk '{print $3}')
-    tmp_interface=$(ip netns exec $ns ip a | egrep "vlan.*if" | awk '{print $2}' | sed 's/\@.*//')
-    tmp_vlan=$(ip netns exec $ns ip a | egrep "vlan.*if" | awk '{print $2}' | sed 's/vlan\.//' | sed 's/\@.*//')
-    tmp_ipaddress=$(ip netns exec $ns ip a | egrep "inet 192" | awk '{print $2}')
     if [[ ( "$interface" != "$tmp_interface" ) || ( "$vlan" != "$tmp_vlan" ) || ( "$ipaddress" != "$tmp_ipaddress" ) ]]
     then
-      printf "\n******* An interface in the $ns network namespace has changed. *******\n"
-      printf "\n************************ Reconfiguring **********************\n"
+      printf "\n***************************************************************************************\n"
+      printf "\n**                 An interface in the $ns network namespace has changed.            **\n"
+      printf "\n***************************************************************************************\n"
+      printf "\n***************************************************************************************\n"
+      printf "\n**                                  Reconfiguring                                    **\n"
+      printf "\n***************************************************************************************\n"
       edit_netns_interface "$ns" "$interface" "$vlan" "$ipaddress"
-      printf "\n******* The $interface interafce in the $ns network namespace was reconfigured. *******\n\n"
+      printf "\n***************************************************************************************\n"
+      printf "\n**      The $interface interafce in the $ns network namespace was reconfigured.      **\n"
+      printf "\n***************************************************************************************\n\n"
     else
-      printf "\n******* The $ns network namespace already exists and no reconfiguration is needed. *******\n"
-      printf "\n*************************** Skipping ****************************\n\n"
+      printf "\n***************************************************************************************\n"
+      printf "\n**      The $ns network namespace already exists and no reconfiguration is needed.   **\n"
+      printf "\n***************************************************************************************\n"
+      printf "\n***************************************************************************************\n"
+      printf "\n**                                    Skipping...                                    **\n"
+      printf "\n***************************************************************************************\n\n"
     fi
     done < /home/lab/ns/$ns
     continue
@@ -61,9 +73,15 @@ for f in $NETNS_FILES; do
     interface=$(echo $line | awk '{print $1}')
     vlan=$(echo $line | awk '{print $2}')
     ipaddress=$(echo $line | awk '{print $3}')
-    printf "\n******* Creatig and configuring the $ns network namespace. *******\n"
+    printf "\n***************************************************************************************\n"
+    printf "\n**                 Creatig and configuring the $ns network namespace.                **\n"
+    printf "\n***************************************************************************************\n"
     create_configure_netns "$ns" "$interface" "$vlan" "$ipaddress"
-    printf "\n******* Configured $interface interface in $ns network namespace *******\n"
-    printf "\n******* $interface interface is in $ns network namespace and has $ipaddress address *******\n\n"
+    printf "\n***************************************************************************************\n"
+    printf "\n**             Configured $interface interface in $ns network namespace.             **\n"
+    printf "\n***************************************************************************************\n"
+    printf "\n***************************************************************************************\n"
+    printf "\n**    $interface interface is in $ns network namespace and has $ipaddress address    **\n"
+    printf "\n***************************************************************************************\n\n"
   done < /home/lab/ns/$ns
 done
